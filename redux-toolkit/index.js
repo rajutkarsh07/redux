dbfc2f95405c5494003c32d66d5a7d279c4b99d0
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-05-19 02:05:31 +0530

    fetching data in redux-toolkit

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,18 +1,22 @@
 const store = require("./app/store");
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { icecreamActions } = require("./features/icecream/icecreamSlice");
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("initial state ", store.getState());
 
 const unsubscribe = store.subscribe(() => {
   //logger middle will do the same what the below code should have done
-  // console.log("updated state ", store.getState());
+  console.log("updated state ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(4));
+store.dispatch(fetchUsers());
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(7));
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(4));
+
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(7));
+
+// unsubscribe();
