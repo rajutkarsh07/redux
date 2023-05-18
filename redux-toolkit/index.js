245e627c0fe88f0cb95d1279e7414ba5cc4dce92
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-05-19 01:22:15 +0530

    made cakeSlice for redux toolkit

---
+++ redux-toolkit/index.js
@@ -0,0 +1,15 @@
+const store = require("./app/store");
+const { cakeActions } = require("./features/cake/cakeSlice");
+
+console.log("initial state ", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("updated state ", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(4));
+
+unsubscribe();
